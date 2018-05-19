9e3849766aa13ff5c45f735faffebb6a98d950ec
antonb <it1@domreklamy.com.ua>
2018-05-20 00:15:41 +0300

    Wipe all

--- Electronic_Timetable/BusinesLogicDLL/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace BusinesLogicDLL
-{
-    public class Class1
-    {
-    }
-}
--- Electronic_Timetable/DataLayerLib/Class1.cs
+++
@@ -1,8 +0,0 @@
-﻿using System;
-
-namespace DataLayerLib
-{
-    public class Class1
-    {
-    }
-}
